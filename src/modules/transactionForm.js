3d67e515d483fc31b7451f63b0bbff32449e67ef
pkimbrel <pkimbrel@gmail.com>
2014-04-12 01:06:33 -0500

    Getting the hang of AngularJS form validatin (sheesh)

--- src/modules/transactionForm.js
+++ src/modules/transactionForm.js
@@ -10,8 +10,26 @@ pkfinance.controller('TransactionForm', ['$scope', '$q', 'validators', 'dataAcce
             "cleared": false,
             "amount": ""
         };
+
         $scope.submit = function () {
             console.log("here");
         };
     }
 ]);
+
+pkfinance.directive('pkDate', function (validators) {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue) {
+                if (moment(viewValue).isValid()) {
+                    ctrl.$setValidity('pkDate', true);
+                    return viewValue;
+                } else {
+                    ctrl.$setValidity('pkDate', false);
+                    return undefined;
+                }
+            });
+        }
+    };
+});
