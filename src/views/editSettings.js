c5938fd9178ca02333335f1c452433b5d1413d36
pkimbrel <pkimbrel@gmail.com>
2014-05-11 20:40:14 -0500

    Working on settings

--- src/views/editSettings.js
+++ src/views/editSettings.js
@@ -3,16 +3,45 @@ pkfinance.controller('Settings', ['$rootScope', '$scope', '$state', '$q', 'valid
         $scope.app = applicationScope;
         $scope.originalSettings = settings.readAllSettings;
         $scope.newSettings = {};
+        $scope.isUpdating = false;
         
         $scope.$watch("originalSettings()", function (updatedSettings) {
             $scope.newSettings = angular.copy(updatedSettings);
         });
         
         $scope.submit = function () {
-            
+            $scope.isUpdating = true;
         };
         
-        $('.help').popover({selector: '[rel=popover]'});
-        $('.help').popover('show');
+        $scope.addBlackout = function () {
+            $scope.newSettings.blackoutPositions.push({
+                "latitude": "0",
+                "longitude": "0"
+            });
+        };
+        
+        $scope.removeBlackout = function (index) {
+            if (confirm("Are you sure?")) {
+                $scope.newSettings.blackoutPositions.splice(index, 1);
+            }
+        };
+        
+        $scope.locateBlackout = function (index) {
+            dataAccessor.getPosition().then(function (currentPosition) {
+                $scope.newSettings.blackoutPositions[index] = currentPosition;
+            });
+
+        };
+        
+        $scope.cancel = function () {
+            $state.transitionTo($rootScope.previousState);
+        };
+        
+        $scope.isSame = function () {
+            var originalSettings = $scope.originalSettings();
+            return angular.equals(originalSettings, $scope.newSettings);
+        };
+        
+        $('.help').popover();
     }
 ]);
