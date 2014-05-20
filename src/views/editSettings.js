d9b387442188b547cdba229f1d19297f11d7d0a8
pkimbrel <pkimbrel@gmail.com>
2014-05-20 00:23:03 -0500

    Added map to blackout list in settings

--- src/views/editSettings.js
+++ src/views/editSettings.js
@@ -4,44 +4,86 @@ pkfinance.controller('Settings', ['$rootScope', '$scope', '$state', '$q', 'valid
         $scope.originalSettings = settings.readAllSettings;
         $scope.newSettings = {};
         $scope.isUpdating = false;
-        
+
         $scope.$watch("originalSettings()", function (updatedSettings) {
             $scope.newSettings = angular.copy(updatedSettings);
         });
-        
+
         $scope.submit = function () {
             $scope.isUpdating = true;
-            settings.updateSettings($scope.newSettings).then(function() {
+            settings.updateSettings($scope.newSettings).then(function () {
                 $state.transitionTo($rootScope.previousState);
-            }).catch(function() {
+            }).catch(function () {
                 alert("Update failed");
                 $scope.isUpdating = false;
             });
         };
-        
+
         $scope.addBlackout = function () {
             if ($scope.newSettings.blackoutPositions === undefined) {
                 $scope.newSettings.blackoutPositions = [];
             }
-            
+
             $scope.newSettings.blackoutPositions.push({
                 "latitude": "0",
                 "longitude": "0"
             });
         };
-        
+
         $scope.removeBlackout = function (index) {
             if (confirm("Are you sure?")) {
                 $scope.newSettings.blackoutPositions.splice(index, 1);
             }
         };
-        
+
         $scope.locateBlackout = function (index) {
             dataAccessor.getPosition().then(function (currentPosition) {
                 $scope.newSettings.blackoutPositions[index] = currentPosition;
             });
         };
-        
+
+        $scope.mapBlackout = function (index) {
+            var blackoutPosition = $scope.newSettings.blackoutPositions[index];
+            var position = new google.maps.LatLng(blackoutPosition.latitude, blackoutPosition.longitude);
+
+            if ($scope.map === undefined) {
+                $("#blackout-map").html("<div id='map-canvas'>");
+                var mapOptions = {
+                    center: new google.maps.LatLng(39.810556, -98.556111),
+                    zoom: 4
+                };
+
+                $scope.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+            }
+
+            $scope.map.setCenter(position);
+            $scope.map.setZoom(16);
+
+            if ($scope.blackoutMarker === undefined) {
+                $scope.blackoutMarker = new google.maps.Marker({
+                    position: position,
+                    map: $scope.map,
+                    title: "Blacked out"
+                });
+                var radiusOptions = {
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.6,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.15,
+                    map: $scope.map,
+                    center: position,
+                    radius: $scope.newSettings.suggestionRadius
+                };
+                $scope.blackoutRadius = new google.maps.Circle(radiusOptions);
+
+            } else {
+                $scope.blackoutMarker.setPosition(position);
+                $scope.blackoutRadius.setCenter(position);
+                $scope.blackoutRadius.setRadius($scope.newSettings.suggestionRadius);
+            }
+        };
+
         $scope.addSummaryCategory = function () {
             if ($scope.newSettings.summaryCategories === undefined) {
                 $scope.newSettings.summaryCategories = [];
@@ -52,38 +94,37 @@ pkfinance.controller('Settings', ['$rootScope', '$scope', '$state', '$q', 'valid
             }
             $scope.newSettings.summaryCategories.push("");
         };
-        
+
         $scope.removeSummaryCategory = function (index) {
             if (confirm("Are you sure?")) {
                 $scope.newSettings.summaryCategories.splice(index, 1);
             }
         };
-        
-        $scope.moveUpSummaryCategory = function(index) {
+
+        $scope.moveUpSummaryCategory = function (index) {
             if (index > 0) {
-                $scope.newSettings.summaryCategories.swapItems(index, index-1);
+                $scope.newSettings.summaryCategories.swapItems(index, index - 1);
             }
         };
-        
-        $scope.moveDownSummaryCategory = function(index) {
+
+        $scope.moveDownSummaryCategory = function (index) {
             if (index < ($scope.newSettings.summaryCategories.length - 1)) {
-                $scope.newSettings.summaryCategories.swapItems(index, index+1);
+                $scope.newSettings.summaryCategories.swapItems(index, index + 1);
             }
         };
-        
-        $scope.changeSummaryCategories = function() {
+
+        $scope.changeSummaryCategories = function () {
             console.log("HERE");
         };
-        
+
         $scope.cancel = function () {
             $state.transitionTo($rootScope.previousState);
         };
-        
+
         $scope.isSame = function () {
             var originalSettings = $scope.originalSettings();
             return angular.equals(originalSettings, $scope.newSettings);
         };
-        
+
         $('.help').popover();
-    }
-]);
+}]);
