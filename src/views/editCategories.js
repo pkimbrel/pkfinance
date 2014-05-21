00eae9eaadf64c9a3ff03e6d3a152dac22bb2c95
Paul Kimbrel <paul@paulkimbrel.com>
2014-05-20 22:25:47 -0500

    Adding categories and typeahead editor

---
+++ src/views/editCategories.js
@@ -0,0 +1,6 @@
+pkfinance.controller('Categories', ['$rootScope', '$scope', '$state', '$q', 'validators', 'dataAccessor', 'applicationScope', 'settings', 'geoServices',
+    function ($rootScope, $scope, $state, $q, validators, dataAccessor, applicationScope, settings, geoServices) {
+        $scope.app = applicationScope;
+        $scope.newCategories = angular.copy(applicationScope.categories);
+    }
+]);
