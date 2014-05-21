e3115b1ba6f6e33a9cb3c43b3101b951709254d0
Paul Kimbrel <pkimbrel@gmail.com>
2014-05-21 14:59:55 -0400

    Adding move/remove functionality to category editor

--- src/views/editCategories.js
+++ src/views/editCategories.js
@@ -72,5 +72,65 @@ pkfinance.controller('Categories', ['$rootScope', '$scope', '$state', '$q', 'val
                 $scope.newCategories = angular.copy(applicationScope.categories);
             }
         });
+        
+        $scope.moveUpParentCategory = function(index) {
+            if (index > 0) {
+                $scope.newCategories.swapItems(index, index - 1);
+            }
+        };
+
+        $scope.moveDownParentCategory = function(index) {
+            if (index < ($scope.newCategories.length - 1)) {
+                $scope.newCategories.swapItems(index, index + 1);
+            }
+        };
+
+        $scope.removeParentCategory = function(index) {
+            if (confirm("Are you ABSOLUTELY sure?")) {
+                $scope.newCategories.splice(index, 1);
+            }
+        };
+        
+        $scope.addParentCategory = function() {
+            $scope.newCategories.push({
+                "text": "New Category",
+                "children": []
+            });
+        };
+
+        $scope.moveUpCategory = function(parentCategory, index) {
+            if (index > 0) {
+                angular.forEach($scope.newCategories, function(parentObj) {
+                    if (parentObj.text == parentCategory) {
+                        parentObj.children.swapItems(index, index - 1);
+                    }
+                });
+            }
+        };
+
+        $scope.moveDownCategory = function(parentCategory, index) {
+            angular.forEach($scope.newCategories, function(parentObj) {
+                if (parentObj.text == parentCategory) {
+                    if (index < (parentObj.children.length - 1)) {
+                        parentObj.children.swapItems(index, index + 1);
+                    }
+                }
+            });
+        };
+
+        $scope.removeCategory = function(parentCategory, index) {
+            if (confirm("Are you ABSOLUTELY sure?")) {
+                angular.forEach($scope.newCategories, function(parentObj) {
+                    if (parentObj.text == parentCategory) {
+                        parentObj.children.splice(index, 1);
+                    }
+                });
+            }
+        };
+        
+        $scope.addCategory = function(parentCategory) {
+            
+        };
+
     }
 ]);
