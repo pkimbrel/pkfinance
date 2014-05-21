b3195298d42a53523f3b098637c4add566b1c003
pkimbrel <pkimbrel@gmail.com>
2014-05-21 01:01:10 -0500

    Building category editor

--- src/views/editCategories.js
+++ src/views/editCategories.js
@@ -1,6 +1,76 @@
 pkfinance.controller('Categories', ['$rootScope', '$scope', '$state', '$q', 'validators', 'dataAccessor', 'applicationScope', 'settings', 'geoServices',
     function ($rootScope, $scope, $state, $q, validators, dataAccessor, applicationScope, settings, geoServices) {
         $scope.app = applicationScope;
-        $scope.newCategories = angular.copy(applicationScope.categories);
+
+        $scope.icons = [
+            "asterisk",
+    "euro",
+    "cloud",
+    "envelope",
+    "pencil",
+    "glass",
+    "music",
+    "search",
+    "star",
+    "user",
+    "film",
+    "ok",
+    "remove",
+    "signal",
+    "cog",
+    "trash",
+    "home",
+    "time",
+    "road",
+    "inbox",
+    "repeat",
+    "refresh",
+    "list-alt",
+    "lock",
+    "flag",
+    "headphones",
+    "tag",
+    "book",
+    "bookmark",
+    "print",
+    "camera",
+    "map-marker",
+    "tint",
+    "ban-circle",
+    "gift",
+    "leaf",
+    "fire",
+    "eye-open",
+    "plane",
+    "calendar",
+    "comment",
+    "magnet",
+    "shopping-cart",
+    "bullhorn",
+    "bell",
+    "thumbs-up",
+    "thumbs-down",
+    "globe",
+    "wrench",
+    "briefcase",
+    "paperclip",
+    "heart-empty",
+    "phone",
+    "pushpin",
+    "usd",
+    "send",
+    "credit-card",
+    "cutlery",
+    "earphone",
+    "phone-alt",
+    "tower",
+    "tree-conifer",
+    "tree-deciduous"];
+
+        $scope.$watch("app.categories", function () {
+            if (applicationScope.categories !== undefined) {
+                $scope.newCategories = angular.copy(applicationScope.categories);
+            }
+        });
     }
 ]);
